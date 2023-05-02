4c7bc3c7a689bc5d6cd22dda14648981060a90b0
MatiRivarola <mati.rivarola15@gmail.com>
2023-05-02 17:37:29 -0300

    add optimization whit webpack and more

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Template from './templates/Template.js';
-import "./styles/main.css"
+import "@styles/main.css";
 
+console.log('hello')
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require("dotenv-webpack");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 
 module.exports = {
     mode: "production",
@@ -15,6 +13,8 @@ module.exports = {
         filename: "[name].[contenthash].js",
         assetModuleFilename: "assets/images/[hash][ext][query]"
     },
+    mode: "development",
+    watch: true ,
     resolve: {
         extensions: ['.js'],
         alias:{
@@ -78,13 +78,5 @@ module.exports = {
             ]
         }),
         new Dotenv(),
-        new CleanWebpackPlugin(),
     ],
-    optimization: {
-        minimizer:true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    }
 }
